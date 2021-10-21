5596221532584e4e74a33518b2d9adb00c0cbbff
tomaszkedziora14 <tomaszkedziora14@wp.pl>
2021-10-21 14:14:01 +0200

    added validator to model

--- application/app/models/Users.php
+++ application/app/models/Users.php
@@ -2,6 +2,9 @@
 
 
 use Phalcon\Mvc\Model;
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\Uniqueness;
+use Phalcon\Validation\Validator\Email;
 
 class Users extends Model
 {
@@ -9,4 +12,24 @@ class Users extends Model
     public $name;
     public $last_name;
     public $email;
+
+    public function validation()
+    {
+        $validator = new Validation();
+
+        $validator->add(
+            'email',
+            new Uniqueness(
+                [
+                    'message' => 'The customer email must be unique',
+                ]
+            )
+        );
+
+        $validator->add('email', new Email(array(
+            'message' => 'The e-mail is not valid'
+        )));
+
+        return $this->validate($validator);
+    }
 }
